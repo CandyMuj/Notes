741cda4608009d104f63ea5a073baedac09739f6
MuJ <jx003js@gmail.com>
2019-07-09 17:05:42 +0800

    添加目录 code 用于存放总结的一些代码

---
+++ code/DHTML/c.js
@@ -0,0 +1,15 @@
+var C = {
+    /** 示例id：demo-001
+     * 
+     * 限制输入框输入的内容仅可是 [正数] 并可自定义保留的小数位数
+     * - 自动替换非数字，或不符合小数位数的字符
+     * 
+     * @param {一般为this} dom 
+     * @param {需要保留的小数位数} n 为空：默认两位 \n 为0 或小于0：正整数
+     */
+    onlyNum: function (dom, n) {
+        n = (n == 0 || n) ? n : 2;
+        var reg = new RegExp("\\d+(" + (n > 0 ? "\\.\\d{0," + n + "}" : "") + ")?");
+        dom.value = dom.value.match(reg) ? dom.value.match(reg)[0] : '';
+    },
+}
