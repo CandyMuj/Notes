3d14134c2388741ac3b7755cb6dcbdf096182b3c
MuJ <jx003js@gmail.com>
2019-07-17 10:22:12 +0800

    commit

--- code/DHTML/c.js
+++ code/DHTML/c.js
@@ -4,7 +4,7 @@ var C = {
      * 限制输入框输入的内容仅可是 [正数] 并可自定义保留的小数位数
      * - 自动替换非数字，或不符合小数位数的字符
      * 
-     * @param {一般为this} dom 
+     * @param {一般为this} dom js对象
      * @param {需要保留的小数位数} n 为空：默认两位 \n 为0 或小于0：正整数
      */
     onlyNum: function (dom, n) {
